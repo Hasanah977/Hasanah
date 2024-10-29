0b1ae3fc6f1de3e7f6e28dcc4049567310585596
Hasanah977 <hasanabakri911@gmail.com>
2024-10-29 21:08:37 +0700

    Add files via upload

---
+++ tipe data string.py
@@ -0,0 +1,13 @@
+a = "alam semesta milik tuhan"
+print (a)
+b= 'beserta isi-isinya milik tuhan'
+print (b)
+c = " " " maka kita sebagai ciptaan tuhan tidak boleh melanggar aturanya , dan wajib menjalankan aturanya" " "
+asil = a + b + c
+print (hasil)
+
+#hasil : Operasi Penyambungan String (string concatenation).
+
+#c : menggunakan 3 kali tanda kutip dipakai untuk membuat multiline string, dimana teks yang diinput bisa tersambung ke beberapa baris
+
+#Cara b dan a tidak ada perbedaan mendasar dan kita bisa memilih ingin menggunakan yang mana saja.
